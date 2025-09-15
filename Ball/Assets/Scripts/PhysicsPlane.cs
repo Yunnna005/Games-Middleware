44414961eeac08d5849a65c61e3794ed51f1d609
Yunnna005 <117185683+Yunnna005@users.noreply.github.com>
2025-09-15 10:54:35 +0100

    Master (#2)
    * Created Utils script; updated the sphere and plane scripts
    
    Signed-off-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>
    
    * Updated the style of get, set
    
    Signed-off-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>
    
    ---------
    
    Signed-off-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>
    Co-authored-by: Student Anna Kovalenko (TL_KCOMP_B_Y4) <t00243622@mymtu.ie>

--- Ball/Assets/Scripts/PhysicsPlane.cs
+++ Ball/Assets/Scripts/PhysicsPlane.cs
@@ -3,11 +3,19 @@ using static Assets.Scripts.Utils;
 
 public class PhysicsPlane : MonoBehaviour
 {
-    public Vector3 normal { get { return transform.up; } internal set { transform.up = value; } }
+    public Vector3 normal { 
+        get 
+        { 
+            return transform.up; 
+        } 
+        internal set 
+        { 
+            transform.up = value; 
+        } 
+    }
 
     internal bool isCollidingWith(PhysicsSphere sphere)
     {
-      
         Vector3 v = sphere.transform.position - this.transform.position;
         Vector3 p = parallelTo(v, normal);
         return p.magnitude < sphere.Radius;
