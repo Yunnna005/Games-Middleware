ca7665ca20a0012277f133c20c7bf38dfecf3f3e
Yunnna005 <annakovalenko0500@gmail.com>
2025-10-15 11:14:26 +0100

    Add new input system and blend tree for moving forward, forward left/right
    Signed-off-by: Yunnna005 <annakovalenko0500@gmail.com>

--- Ball/Assets/Aminations/Scripts/EdController.cs
+++ Ball/Assets/Aminations/Scripts/EdController.cs
@@ -1,17 +1,54 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EdController : MonoBehaviour
 {
-    public Animator animator;
+    Animator animator;
+    CharacterController controller;
+
+    private float speed = 2f;
+    private float jumpHight = 2f;
+    private float gravity = -9.81f;
+
+    Vector2 moveInput;
+    Vector3 velocity;
+
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {moveInput}");
+
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Jumpping: {context.performed} - Is Grounded: {controller.isGrounded}");
+        if (context.performed && controller.isGrounded)
+        {
+            Debug.Log("Character is supposed to jump");
+            velocity.y = Mathf.Sqrt(jumpHight * -2f * gravity);
+        }
     }
 
     void Update()
     {
+        //Movement logic
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * speed * Time.deltaTime);
 
-        animator.SetBool("isWalking", Input.GetKey(KeyCode.W));
-      
+        //Jump logic
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        animator.SetBool("isWalking", moveInput != Vector2.zero);
+        animator.SetFloat("VelocityX", moveInput.x );
+        animator.SetFloat("VelocityZ", moveInput.y);
     }
 }
