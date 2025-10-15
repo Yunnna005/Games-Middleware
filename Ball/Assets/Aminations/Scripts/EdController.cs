3ca074253e7a3f3395918bd49c7eb5358cbfd5a8
Yunnna005 <annakovalenko0500@gmail.com>
2025-10-15 18:35:10 +0100

    Created Jump Blend Tree. Issues with Jump animations
    Signed-off-by: Yunnna005 <annakovalenko0500@gmail.com>

--- Ball/Assets/Aminations/Scripts/EdController.cs
+++ Ball/Assets/Aminations/Scripts/EdController.cs
@@ -7,12 +7,12 @@ public class EdController : MonoBehaviour
     CharacterController controller;
 
     private float speed = 2f;
-    private float jumpHight = 2f;
+    private float jumpHeight = 3f;
     private float gravity = -9.81f;
 
     Vector2 moveInput;
     Vector3 velocity;
-
+    private bool isJumping;
 
     private void Start()
     {
@@ -23,22 +23,34 @@ public class EdController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
         Debug.Log($"Move Input: {moveInput}");
-
-
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         Debug.Log($"Jumpping: {context.performed} - Is Grounded: {controller.isGrounded}");
-        if (context.performed && controller.isGrounded)
+        bool isGrounded = controller.isGrounded || velocity.y <= 0.1f;
+
+        if (context.performed && isGrounded)
         {
             Debug.Log("Character is supposed to jump");
-            velocity.y = Mathf.Sqrt(jumpHight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+            animator.SetFloat("JumpVelocity", 1f);
         }
     }
 
     void Update()
     {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (controller.isGrounded && isJumping && velocity.y < 0)
+        {
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
         //Movement logic
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
         controller.Move(move * speed * Time.deltaTime);
@@ -47,8 +59,17 @@ public class EdController : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
+        float velocityX = Mathf.Round(moveInput.x);
+        float velocityZ = Mathf.Round(moveInput.y);
+
         animator.SetBool("isWalking", moveInput != Vector2.zero);
         animator.SetFloat("VelocityX", moveInput.x );
         animator.SetFloat("VelocityZ", moveInput.y);
+
+        if (isJumping)
+        {
+            float normalizedVelocity = Mathf.Clamp(velocity.y / 10f, -1f, 1f);
+            animator.SetFloat("JumpVelocity", normalizedVelocity);
+        }
     }
 }
